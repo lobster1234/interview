803d128f0c728fd1a58d4211164c7b33bacc4fbe
mpandit <lobster1234@yahoo.com>
2015-01-27 03:38:33 -0800

    Added getFactorial in scala

--- src/main/scala/com/manish/interview/NumberQuestionsS.scala
+++ src/main/scala/com/manish/interview/NumberQuestionsS.scala
@@ -11,11 +11,18 @@ object NumberQuestionsS extends App{
       println(fib(i))
   }
 
+  for(i <-0 to 10){
+    println(s"""Factorial of $i is ${factorial(i)}""")
+  }
+
   def fib(n:Int) : Int = {
     if(n <= 2) 1
     else fib(n-2) + fib(n-1)
   }
 
 
+  def factorial(n:Int) : Int = {
+    if(n == 1) 1 else if (n==0) 0 else n*factorial(n-1)
+  }
 
 }
